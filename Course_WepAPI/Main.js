555e9936e44b00a319597671312d43d8748985a3
kpnemre <kpnemre51@gmail.com>
2020-10-24 19:20:17 +0300

    promise

--- Course_WepAPI/Main.js
+++ Course_WepAPI/Main.js
@@ -1,38 +1,67 @@
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, View, Text, Alert, FlatList, StyleSheet, TextInput,Button } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Alert,
+  FlatList,
+  StyleSheet,
+  TextInput,
+  Button,
+} from 'react-native';
 import axios from 'axios';
 
-
-
-
 const Main = (props) => {
+  const fetchData_Then = () => {
+    let myData = [];
 
-    const fetchData=()=>{
-        console.log('startig fetch....');
+    console.log('startig fetch....');
 
-        // const myData=axios.get('https://jsonplaceholder.typicode.com/users')
-        // console.log(myData);
-        const myData=axios.get('https://jsonplaceholder.typicode.com/users')
-        .then((response)=>{
-            console.log(response);
-        })
-        .catch((error)=>{
-            console.log(error);
-        })
-        // data gelmsei bittikten sonra tetiklenir then bloğu.
+    // const myData=axios.get('https://jsonplaceholder.typicode.com/users')
+    // console.log(myData);
+    axios.get('https://jsonplaceholder.typicode.com/users').then((response) => {
+      console.log(response);
+      myData = response.data;
+    });
+    console.log(myData);
+    // myData boşgelir. çünkü then işlem bitmeden tetiklenmez.
+    console.log('end fetch..');
+    // .catch((error)=>{
+    //     console.log(error);
+    // })
 
-        console.log('ending fetch ...')
+    // data gelmsei bittikten sonra tetiklenir then bloğu.
 
-    }
+    console.log('ending fetch ...');
+  };
 
-    return(
-        <SafeAreaView>
-            <View>
-                <Text>Main</Text>
-                <Button title='Fetch Data' onPress={fetchData}/>
-            </View>
-        </SafeAreaView>
-    )
-}
+  const fetchData_Await = async () => {
+    console.log('starting await fetch....');
+
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/users',
+    );
+    console.log(response);
+    console.log('end await fetch....')
+  };
+
+  return (
+    // <SafeAreaView>
+    //     <View>
+    //         <Text>Main</Text>
+    //         <Button title='Fetch Data' onPress={fetchData_Then}/>
+    //     </View>
+    // </SafeAreaView>
+
+    <SafeAreaView>
+      <View>
+        <Text>Main</Text>
+        <Button title="Fetch Data Then" onPress={fetchData_Then} />
+        <Text></Text>
+        <Button title="Fetch Data Await" onPress={fetchData_Await} />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default Main;
---
+++ Course_WepAPI/Main2.js
@@ -0,0 +1,90 @@
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Alert,
+  FlatList,
+  StyleSheet,
+  TextInput,
+  Button,
+} from 'react-native';
+import axios from 'axios';
+
+const Main = (props) => {
+
+    const[userData, setUserData] = useState([]);
+
+  const fetchData_Then = () => {
+
+//     const {data} =  axios.get(
+//         'https://jsonplaceholder.typicode.com/users',
+//       );
+//   setUserData(data);
+
+    axios.get('https://jsonplaceholder.typicode.com/users').then((response) => {
+    //   setUserData(response.data)
+    
+    setUserData(response.data); 
+    console.log(response.data);
+    })
+  }
+
+  const fetchData_Await = async () => {
+    const {data} = await axios.get(
+      'https://jsonplaceholder.typicode.com/users',
+    );
+setUserData(data);
+// setUserData(response.data); const (response)
+// async yapılarda return yaplmaz. burda state güncellemesi yaptık
+  };
+
+
+  function promiseFunction(number) {
+    return new Promise((resolve, reject) => {
+        if (number > 5) {
+            resolve("Sayı beşten büyük!");
+        }
+        else {
+            reject("Sayı beşten küçük..");
+        }
+    })
+}
+
+const checkNumber = () => {
+    promiseFunction(1)
+        .then(response => {
+            console.log("response: ");
+            console.log(response);
+        })
+        .catch(error => {
+            console.log("error: ");
+            console.log(error);
+        })
+}
+
+
+  return (
+
+
+    <SafeAreaView>
+      <View>
+        <Text>Main</Text>
+        <Button title="Fetch Data Then" onPress={fetchData_Then} />
+        <Text></Text>
+        <Button title="Fetch Data Await" onPress={fetchData_Await} />
+        <Text></Text>
+
+        <Button title="Number" onPress={checkNumber} />
+
+        <FlatList 
+        data={userData}
+  renderItem={({item})=> <Text>{item.name}</Text>}
+        />
+      </View>
+    </SafeAreaView>
+  )
+
+};
+
+export default Main;
